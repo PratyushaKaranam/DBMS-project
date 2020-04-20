7fbcbebcf147539821b1fcda02b2863917d3fb28
Daniel Quintero <danqmail@gmail.com>
2020-04-20 18:40:23 -0400

    reorganization and changes

--- trendviewer.js
+++ trendviewer.js
@@ -10,7 +10,9 @@ const endyear = enddate[0], endmonth = enddate[1], endday = enddate[2];
 
 //temporary global variables for testing
 var arr1, arr2, arr3;
+
 var charts = new Array();
+var stages = new Array();
 
 $(document).ready(function () {
     switch (type) {
@@ -30,7 +32,6 @@ $(document).ready(function () {
 })
 
 async function queryServer() {
-    console.log("we get here");
     const result = await $.ajax(
         {
             url: "/api/test/trends?type=" + type
@@ -43,83 +44,115 @@ async function queryServer() {
             type: "GET",
         }
     )
-    console.log(result);
+    // console.log(result);
 
     return result;
 }
 
 function saveChartPDF(chart_number) {
-    charts[chart_number].saveAsPdf('a2', true, 5, 5, 'Chart #' + chart_number);
+    charts[chart_number].saveAsPdf('a2', true, 5, 5, 'Chart_' + chart_number);
+}
+
+function switchLayer(stage_number) {
+    stages[stage_number].stage.suspend();
+    for (i = 0; i < stages[stage_number].layers.length; i++) {
+        stages[stage_number].layers[i].zIndex(0);
+    }
+    stages[stage_number].active++;
+    if (stages[stage_number].active == stages[stage_number].layers.length) {
+        stages[stage_number].active = 0;
+    }
+
+    stages[stage_number].layers[stages[stage_number].active].zIndex(1000000);
+    stages[stage_number].stage.resume();
 }
 
 async function buildFinancialCharts() {
     financialSummaryData = await queryServer();
-    arr1 = financialSummaryData.Days
-    arr2 = financialSummaryData.GrossRevPerDayYellow
-    arr3 = financialSummaryData.GrossRevPerDayGreen
 
-    await charts.push(
+    // console.log(financialSummaryData);
+
+    stage0 = anychart.graphics.create("chart0")
+    s0layer0 = stage0.layer();
+    s0layer0.zIndex(80);
+    s0layer1 = stage0.layer();
+    s0layer1.zIndex(100);
+    stages.push({ stage: stage0, active: 0, layers: [s0layer0, s0layer1] });
+
+    charts.push(
         createLineChart(
             "Daily average of total payments",
             "Days",
             "Daily average",
-            getDataset(arr1, arr2, arr3))
+            getDataset(financialSummaryData.StatisticsPerDay.Days,
+                financialSummaryData.StatisticsPerDay.AverageTotal.YellowAvg,
+                financialSummaryData.StatisticsPerDay.AverageTotal.GreenAvg),
+            { slabels: ["Yellow Taxi", "Green Taxi"], colors: ["gold", "green"] }
+        ).container(s0layer0)
     );
-    console.log(charts.length);
-
-
-}
-
-async function buildTripCharts() {
-    tripSummaryData = await queryServer();
-
-
-    // await charts.push(
-    //     createLineChart(
-    //         "Daily average of total payments",
-    //         "Days",
-    //         "Daily average",
-    //         getDataset(arr1, arr2, arr3))
-    // );
 
 
     charts.push(
-        createPieChart(
-            "Top Early Pickup Locations",
-            getPieDataset(
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Pickup.Zone,
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Pickup.Count,
-            ), 1
-        )
-    )
-    charts.push(
-        createPieChart(
-            "Top Early Dropoff Locations",
-            getPieDataset(
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Dropoff.Zone,
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Dropoff.Count,
-            ), 2
-        )
-    )
-    charts.push(
-        createPieChart(
-            "Top Late Pickup Locations",
-            getPieDataset(
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Pickup.Zone,
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Pickup.Count,
-            ), 3
-        )
-    )
+        createLineChart(
+            "Daily median of total payments",
+            "Days",
+            "Daily median",
+            getDataset(financialSummaryData.StatisticsPerDay.Days,
+                financialSummaryData.StatisticsPerDay.MedianTotal.YellowMedian,
+                financialSummaryData.StatisticsPerDay.MedianTotal.GreenMedian),
+            { slabels: ["Yellow Taxi", "Green Taxi"], colors: ["gold", "green"] }
+        ).container(s0layer1)
+    );
+
+    stage1 = anychart.graphics.create("chart1")
+    s1layer0 = stage1.layer();
+    s1layer0.zIndex(80);
+    s1layer1 = stage1.layer();
+    s1layer1.zIndex(100);
+    stages.push({ stage: stage1, active: 0, layers: [s1layer0, s1layer1] });
 
     charts.push(
-        createPieChart(
-            "Top Late Dropoff Locations",
-            getPieDataset(
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Dropoff.Zone,
-                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Dropoff.Count,
-            ), 4
-        )
-    )
+        createLineChart(
+            "Daily average of total payments in Manhattan vs Not Manhattan",
+            "Days",
+            "Daily average",
+            getDataset(financialSummaryData.StatisticsPerDay.Days,
+                financialSummaryData.StatisticsPerDay.AverageTotal.ManhattanAvg,
+                financialSummaryData.StatisticsPerDay.AverageTotal.NotManhattanAvg),
+            { slabels: ["Manhattan", "Other Boroughs"], }
+        ).container(s1layer0)
+    );
+
+    charts.push(
+        createLineChart(
+            "Daily median of total payments in Manhattan vs Not Manhattan",
+            "Days",
+            "Daily median",
+            getDataset(financialSummaryData.StatisticsPerDay.Days,
+                financialSummaryData.StatisticsPerDay.MedianTotal.ManhattanMedian,
+                financialSummaryData.StatisticsPerDay.MedianTotal.NotManhattanMedian),
+            { slabels: ["Manhattan", "Other Boroughs"], }
+        ).container(s1layer1)
+    );
+
+    stage2 = anychart.graphics.create("chart2")
+    s2layer0 = stage2.layer();
+    s2layer0.zIndex(80);
+    s2layer1 = stage2.layer();
+    s2layer1.zIndex(100);
+    stages.push({ stage: stage2, active: 0, layers: [s2layer0, s2layer1] });
+
+    charts.push(
+        createLineChart(
+            "Total Tips:Passenger Count ratio as a function of rounded trip distance",
+            "Trip distance (Rounded)",
+            "Tip per passenger",
+            getDataset(financialSummaryData.TipsAsFunctionOfDistance.Distances,
+                financialSummaryData.TipsAsFunctionOfDistance.Values,
+                undefined),
+            { slabels: ["Tips"] }
+        ).container(s2layer0)
+    );
 
     document.getElementById("dateinfo").innerHTML =
         'Date in range:<br />' +
@@ -130,10 +163,81 @@ async function buildTripCharts() {
         `
         <div>
             Summary Information <br />
-            Most pickups: ${tripSummaryData.MostPickups}
+            Most pickups: ${financialSummaryData.placeholder}
         </div>
         `
-    console.log(charts.length);
+
+}
+
+async function buildTripCharts() {
+    tripSummaryData = await queryServer();
+    stage0 = anychart.graphics.create("chart0")
+    s0layer0 = stage0.layer();
+    s0layer0.zIndex(80);
+    s0layer1 = stage0.layer();
+    s0layer1.zIndex(100);
+    stages.push({ stage: stage0, active: 0, layers: [s0layer0, s0layer1] });
+
+    charts.push(
+        createPieChart(
+            "Top Early Pickup Locations",
+            getPieDataset(
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Pickup.Zone,
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Pickup.Count,
+            ), 1
+        ).container(s0layer0)
+    )
+    charts.push(
+        createPieChart(
+            "Top Early Dropoff Locations",
+            getPieDataset(
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Dropoff.Zone,
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopEarlyLocations.Dropoff.Count,
+            ), 2
+        ).container(s0layer1)
+    )
+
+    stage1 = anychart.graphics.create("chart1")
+    s1layer0 = stage1.layer();
+    s1layer0.zIndex(80);
+    s1layer1 = stage1.layer();
+    s1layer1.zIndex(100);
+    stages.push({ stage: stage1, active: 0, layers: [s1layer0, s1layer1] });
+
+    charts.push(
+        createPieChart(
+            "Top Late Pickup Locations",
+            getPieDataset(
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Pickup.Zone,
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Pickup.Count,
+            ), 3
+        ).container(s1layer0)
+    )
+
+    charts.push(
+        createPieChart(
+            "Top Late Dropoff Locations",
+            getPieDataset(
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Dropoff.Zone,
+                tripSummaryData.BoroughSummary.TopLocationsByTime.TopLateLocations.Dropoff.Count,
+            ), 4
+        ).container(s1layer1)
+    )
+
+    document.getElementById("dateinfo").innerHTML =
+        'Date in range:<br />' +
+        'Start: ' + startdate.join('/') + '<br />' +
+        'End: ' + enddate.join('/')
+
+    // document.getElementById('summary').innerHTML =
+
+    //     `
+    //     <div>
+    //         Summary Information <br />
+    //         Most pickups: ${tripSummaryData.MostPickups}
+    //     </div>
+    //     `
+    // console.log(charts.length);
 }
 
 function drawCharts() {
@@ -152,8 +256,7 @@ function drawCharts() {
 
     for (i = 0; i < charts.length; i++) {
         // set container id for the chart
-        charts[i].container("chart" + i);
-        console.log("NANIT")
+        // charts[i].container("chart" + i);
         charts[i].draw();
     }
 
@@ -170,7 +273,7 @@ function getDataset(xData, yData1, yData2) {
     for (i = 0; i < dataset.length; i++) {
         dataset[i][0] = xData[i];
         dataset[i][1] = yData1[i];
-        dataset[i][2] = yData2[i];
+        dataset[i][2] = yData2 == undefined ? undefined : yData2[i];
     }
     return anychart.data.set(dataset);
 }
@@ -223,13 +326,11 @@ function createPieChart(title, dataset, order) {
     return chart;
 }
 
-function createLineChart(title, xlabel, ylabel, dataset, bounds) {
+function createLineChart(title, xlabel, ylabel, dataset, { numSeries = 2, slabels = ["1", "2"], colors = ["blue", "orange"] } = {}) {
 
     /* yellow data */
     const seriesData_1 = dataset.mapAs({ 'x': 0, 'value': 1 });
 
-    /* green data */
-    const seriesData_2 = dataset.mapAs({ 'x': 0, 'value': 2 });
 
     /* Create line chart */
     var chart = anychart.line();
@@ -249,18 +350,28 @@ function createLineChart(title, xlabel, ylabel, dataset, bounds) {
     chart.xAxis().title(xlabel);
     chart.yAxis().title(ylabel);
 
+    chart.legend()
+        .enabled(true)
+        .fontSize(13)
+        .padding([0, 0, 10, 0]);
+
+
+
     /* First series configuration */
     let series1 = chart.line(seriesData_1);
+
+    series1.name(slabels[0]);
+
     series1.markers()
         .enabled(true)
         .type('circle')
         .size(1)
         .stroke({
-            color: "gold",
+            color: colors[0],
             thickness: 2
         })
     series1.stroke({
-        color: "gold",
+        color: colors[0],
         thickness: 1
     })
     series1.tooltip()
@@ -269,10 +380,19 @@ function createLineChart(title, xlabel, ylabel, dataset, bounds) {
         .offsetX(5)
         .offsetY(5);
 
+
+    // if (dataset[0] != undefined) {
+
+    /* green data */
+    const seriesData_2 = dataset.mapAs({ 'x': 0, 'value': 2 });
+
     /* Second series configuration */
     let series2 = chart.line(seriesData_2);
+
+    series2.name(slabels[1]);
+
     series2.stroke({
-        color: "green",
+        color: colors[1],
         thickness: 1
     })
     series2.markers()
@@ -280,7 +400,7 @@ function createLineChart(title, xlabel, ylabel, dataset, bounds) {
         .type('circle')
         .size(1)
         .stroke({
-            color: "green",
+            color: colors[1],
             thickness: 2
         });
     series2.tooltip()
@@ -288,132 +408,7 @@ function createLineChart(title, xlabel, ylabel, dataset, bounds) {
         .anchor('left-center')
         .offsetX(5)
         .offsetY(5);
-
-    chart.legend()
-        .enabled(true)
-        .fontSize(13)
-        .padding([0, 0, 10, 0]);
-
+    // }
 
     return chart;
-}
-
-
-/* Unsure if the rest of the code below will be necessary */
-
-// Extracting dates/months from the URL
-
-var URLparameters = location.search.substring(1).split('&');
-var parameterKeyValue;
-//var text = "";
-var label = new Array();
-var parametervalues = new Array();
-var day;
-var month;
-var year;
-// var type = URLparameters[0].split("=");
-// var builder = unescape(type[1]);                                        // builder stores whether tripAndLocation or Financial
-//alert(builder);                                               
-for (var i = 1; i < URLparameters.length; i++) {
-    parameterKeyValue = URLparameters[i].split("=");
-    label.push(unescape(parameterKeyValue[1]));                             // label stores the months or dates to be used in the graph
-    parametervalues.push(unescape(parameterKeyValue[1]));
-    //text += "<br>" + unescape(parameterKeyValue[1]);
-}
-//document.getElementById("result").innerHTML += text;
-if (parameterKeyValue[0] == 'enddate') {                                  // Checking if dates were selected
-    var sdate = new Date(parametervalues[0]);
-    var edate = new Date(parametervalues[1]);
-
-    Date.prototype.addDay = function (days) {                 // This function generates the date after n days
-        var date = new Date(this.valueOf());
-        date.setDate(date.getDate() + days);
-        return date;
-    }
-
-
-    label = getDateArray(sdate.addDay(1), edate.addDay(1));     // label stores the generated dates
-    //alert(label);
-
-    //alert(label);
-    //document.getElementById("result1").innerHTML = label;
-}
-else {
-    var text = "";
-    for (var i = 0; i < label.length; i++) {
-        text += label[i] + "<br>";
-    }
-    // document.getElementById("startdatename").innerHTML = 'SELECTED MONTHS:';
-    // document.getElementById("startdate").innerHTML = text;
-    // document.getElementById("enddatename").style.border = '0px black solid';
-    // document.getElementById("enddate").style.border = '0px black solid';
-}
-
-var dataset1 = new Array();                                   // generates random data for the graph --->NOT IMPORTANT (will be removed later)
-var dataset2 = new Array();
-for (var i = 0; i < label.length; i++) {
-    dataset1.push(Math.floor(Math.random() * label.length));
-    dataset2.push(Math.floor(Math.random() * label.length));
-}
-
-function savePDF1() {
-    chart1.saveAsPdf('a2', true, 5, 5, 'PdfChart1');
-}
-function savePDF2() {
-    chart.saveAsPdf('a2', true, 5, 5, 'PdfChart2');
-}
-
-// creating chart dataset from data
-function getData(arr1, arr2, arr3) {
-    var dataset = new Array(arr1.length);
-    for (i = 0; i < dataset.length; i++) {
-        dataset[i] = new Array(3);
-    }
-    for (i = 0; i < dataset.length; i++) {
-        dataset[i][0] = arr3[i];
-        dataset[i][1] = arr1[i];
-        dataset[i][2] = arr2[i];
-    }
-    // function getData(label,dataset2) {
-    //   var dataset = new Array(label.length);
-    //   for(i=0;i<dataset.length;i++){
-    //     dataset[i] = new Array(2);
-    //   }
-    //   for(i=0;i<dataset.length;i++){
-    //     dataset[i][0] = label[i];
-    //     dataset[i][1] = dataset2[i];
-    //   }
-    console.log(dataset);
-    return dataset;
-}
-
-function getDateArray(start, end) {                       // The function generates dates between Start and End date in MM-DD-YYYY format
-    var arr = new Array();
-    var dt = start;
-    while (dt <= end) {
-        date = new Date(dt);
-        //alert(date);
-        day = date.getDate();
-        //alert(day);
-        month = date.getMonth() + 1;
-        //alert(month);
-        year = date.getFullYear();
-        //alert(year);
-        if (day < 10) {
-            //alert(day);
-            day = '0' + day;
-            //alert(day);
-        }
-        if (month < 10) {
-            //alert(month)
-            month = '0' + month;
-            //alert(month);
-        }
-        var date1 = month + '-' + day + '-' + year;
-        //alert(date1);
-        arr.push(date1);
-        dt = dt.addDay(1);                                     // call to the function addDay to generate the date for the next day
-    }                                                        // this is done till all dates between the start and the end date are generated
-    //alert(arr);
-    return arr;
 }
